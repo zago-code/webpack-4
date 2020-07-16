5426e40d6b098ff464fb4e8c53efab4f603e275b
Gabriel Zarate <gzaratet1@gmail.com>
2020-07-15 23:17:33 -0500

    Working with React

--- react/src/js/components/app.js
+++ react/src/js/components/app.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react';
+
+import Loader from './loader';
+import data from './data.json';
+
+console.log(data);
 
 function App() {
+  const [loaderList, setLoaderList] = useState([]);
+  function handleClick() {
+    setLoaderList(data.loaders);
+  }
   return (
     <div>
       que linda aplicación en React.js
+      <ul>
+        {loaderList.map((item) => (
+          <Loader {...item} key={item.id} />
+        ))}
+      </ul>
+      <button onClick={handleClick}>Mostrar lo aprendido hasta el momento</button>
     </div>
-  )
+  );
 }
 
 export default App;
